9345dc6cf0800c10b6f2f4e01d9ca141ae84ea4a
Bruno <bruno_oliveira1@hotmail.com>
2020-08-31 18:32:47 -0300

    Reformulação das classes - Interface

---
+++ interface/objects.py
@@ -0,0 +1,76 @@
+import tkinter
+
+
+class FrameStreetsButtons:
+
+    def __init__(self, master, qtd, name, row, column):
+        self.qtd = qtd
+        self.name = name
+        self.row = row
+        self.column = column
+
+
+        buttons_frame = tkinter.Frame(master, width = 80, height = 115)
+        buttons_frame.grid(row = self.row, column = self.column, sticky = 's', ipady = 8)
+    
+        for n in range(self.qtd):
+            button = tkinter.Button(buttons_frame, text = f'{self.name}{n + 1}', width = 10)
+            button.grid(row = n, column = 0, padx = 5, pady = 3, sticky = 's')
+
+class FrameStreetsTable:
+
+    def __init__(self, master, rows, table_type, row, column):
+        self.rows = rows
+        self.table_type = table_type
+        self.row = row
+        self.column = column
+
+        title_dict = {
+            'equity': ['Range\n(mãos)', 'Eq. Vilão\n(%)', 'Eq. Hero\n(%)', 'Split\n(%)'],
+            'fold_equity': ['FE/Block\n(mãos)', 'FE/Block\n(%)', 'Cbet\n(%)'],
+        }
+        
+        
+        # main frame
+        range_frame = tkinter.Frame(master, padx = 5, pady = 5, bd = 2, relief = 'groove')
+        range_frame.grid(row = self.row, column = self.column, padx = 10, pady = 10)
+
+        # create the labels with the titles
+        for i in range(len(title_dict[self.table_type])):
+            label = tkinter.Label(range_frame, text = title_dict[self.table_type][i])
+            label.grid(row = 0, column = i, padx = 5)
+
+        # create the entries(?)
+        for i in range(len(title_dict[self.table_type])):
+            for x in range(self.rows):
+                entry = tkinter.Entry(range_frame, width = 8)
+                entry.grid(row = x + 1, column = i, padx = 5, pady = 7)
+
+
+class FrameCards:
+
+    def __init__(self, master, row, column):
+        self.row = row
+        self.column = column
+
+        # main frame
+        main_frame = tkinter.Frame(master, padx = 5, pady = 5, bd = 2, relief = 'groove')
+        main_frame.grid(row = self.row, column = self.column, pady = 10, columnspan = 3)
+
+        labels_list = ['Hero', 'Flop', 'Turn', 'River']
+
+        for i in range (len(labels_list)):
+            frame = tkinter.Frame(main_frame)
+            frame.grid(row = 0, column = i, padx = 5)
+
+            label = tkinter.Label(frame, text = f'{labels_list[i]}:')
+            label.grid(row = 0, column = 0)
+
+            entry = tkinter.Entry(frame, width = 5)
+            entry.grid(row = 0, column = 1, padx = 5)
+
+            button_choose = tkinter.Button(frame, width = 2, height = 1)
+            button_choose.grid(row = 0, column = 2, padx = 5, pady = 5)
+
+            button_clear = tkinter.Button(frame, width = 2)
+            button_clear.grid(row = 0, column = 3)
--- interface/root.py
+++ interface/root.py
@@ -3,19 +3,14 @@ from tkinter import ttk
 
 
 from tab_start import FrameBasicInformations, FrameOpponentInformations, FrameNotesAndInfos
-from tab_pre_flop import FramePreFlopButtons, FramePreFlopEquity, FramePreFlopFoldEquity
-from tab_flop import FrameFlopButtons, FrameFlopEquity, FrameFlopFoldEquity
-from tab_turn import FrameTurnButtons, FrameTurnEquity, FrameTurnFoldEquity
-from tab_river import FrameRiverButtons, FrameRiverEquity, FrameRiverFoldEquity
-
-from other_objects import FrameCards
+from objects import FrameCards, FrameStreetsButtons, FrameStreetsTable
 
 
 # main window
 root = tkinter.Tk()
 root.title("Poker - Análise de Ranges")
-# root.wm_geometry("570x330")
-root.wm_resizable("False", "False")
+# root.wm_geometry("500x330")
+# root.wm_resizable("False", "False")
 
 
 # tabcontrol
@@ -23,47 +18,22 @@ tabcontrol = ttk.Notebook(root)
 tabcontrol.grid(padx = 5, pady = 5)
 
 tab_start = ttk.Frame(root, width = 546, height = 210)
-tab_pre_flop = ttk.Frame(root, width = 546, height = 210)
-tab_flop = ttk.Frame(root, width = 546, height = 210)
-tab_turn = ttk.Frame(root, width = 546, height = 210)
-tab_river = ttk.Frame(root, width = 546, height = 210)
-tab_test = ttk.Frame(root, width = 546, height = 210)
-
 tabcontrol.add(tab_start, text="Início")
-tabcontrol.add(tab_pre_flop, text="Pré-Flop")
-tabcontrol.add(tab_flop, text="Flop")
-tabcontrol.add(tab_turn, text="Turn")
-tabcontrol.add(tab_river, text="River")
-tabcontrol.add(tab_test, text="Teste")
-
-
-# frames
 frame_basic_informations = FrameBasicInformations(tab_start)
 frame_opponent_informations = FrameOpponentInformations(tab_start)
 frame_notes_and_infos = FrameNotesAndInfos(tab_start)
 
-frame_pre_flop_buttons = FramePreFlopButtons(tab_pre_flop)
-frame_pre_flop_equity = FramePreFlopEquity(tab_pre_flop)
-frame_pre_flop_fold_equity = FramePreFlopFoldEquity(tab_pre_flop)
-
-frame_flop_buttons = FrameFlopButtons(tab_flop)
-frame_flop_equity = FrameFlopEquity(tab_flop)
-frame_flop_fold_equity = FrameFlopFoldEquity(tab_flop)
-
-frame_turn_buttons = FrameTurnButtons(tab_turn)
-frame_turn_equity = FrameTurnEquity(tab_turn)
-frame_turn_fold_equity = FrameTurnFoldEquity(tab_turn)
-
-frame_river_buttons = FrameRiverButtons(tab_river)
-frame_river_equity = FrameRiverEquity(tab_river)
-frame_river_fold_equity = FrameRiverFoldEquity(tab_river)
-
-frame_teste = FrameCards(tab_test, 'Hero', 0, 0, 5)
-frame_teste = FrameCards(tab_test, 'Flop', 0, 1, 5)
-frame_teste = FrameCards(tab_test, 'Turn', 0, 2, 5)
-frame_teste = FrameCards(tab_test, 'River', 0, 3, 5)
-
-
+streets = ('Pré-Flop', 2), ('Flop', 3), ('Turn', 3), ('River', 3)
+for tab, row in streets:
+    # frame = ttk.Frame(tabcontrol)
+    main_frame = tkinter.Frame(tabcontrol)
+    inside_frame = tkinter.Frame(main_frame)
+    FrameStreetsButtons(inside_frame, row, 'F', 0, 0)
+    FrameStreetsTable(inside_frame, row, 'equity', 0, 1)
+    FrameStreetsTable(inside_frame, row, 'fold_equity', 0, 2)
+    FrameCards(inside_frame, 1, 0)
+    inside_frame.place(in_=main_frame, anchor="c", relx=.5, rely=.5)
+    tabcontrol.add(main_frame, text = tab)
 
 
 root.mainloop()
